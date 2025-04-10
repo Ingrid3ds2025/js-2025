774c615038adf8146c3a7a5c36fbcf498b9114d6
Ingrid3ds2025 <thamires.carvalho.carneiro@escola.pr.gov.br>
2025-04-10 07:50:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// COLOCANDO AS MENSAGENS EM UM VETOR OU ARRAY
+let messages = [];
+
+function addMessage() {
+    const usernameInput = document.getElementById('username');
+    const messageInput = document.getElementById('message');
+
+    const username = usernameInput.value.trim();
+    const message  = messageInput.value.trim();
+
+    if(!username || !message) {
+        alert('Por favor, preencha todos os campos!');
+        return;
+    }
+
+    // ADICIONAR NOVA MENSAGEM AO VETOR OU ARRAY
+    const newMessage = {
+        username:  username,
+        message:   message,
+        timestamp: new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+    }
+
+    message.push(newMessage);
+
+    //ATUALIZA TABELA
+    updateChat();
+
+    //LIMPAR CAMPOS
+    messageInput.value = '';
+    messageInput.focus();
+}
+
+function updateChat(){
+    const chatBody     = document.getElementById('chat-body');
+    chatBody.innerHTML = '';
+
+    messages.forEach(msg => {
+        const row = document.getElementById('tr');
+        row.innerHTML = `
+            <td> ${msg.username} </td>
+            <td> ${msg.message} </td>
+            <td> class="timestamp" ${msg.timestamp} </td>
+        `;
+        chatBody.appendChild(row);
+    })
+}
+
+//PERMITIR ENVIAR COM A TECLA ENTER
+document.getElementById('message').addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        addMessage();
+    }
+});
